c7a973fceafbe101a9d96bbb0651bf4ce7d92167
David Goza <david@gozasc.com>
2015-01-26 04:22:18 +0000

    update to write config file

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express')
 var bodyParser = require("body-parser");
 var app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
+var fs = require("fs");
 app.use("/", express.static('public'));
-app.post('update',function(request,response){
-var test=request.body.test;
-console.log(test);
-response.send('k');
+
+app.post('/update',function(request,response){
+ var test=request.body.test;
+ fs.writeFile('config.txt', test, function() {
+   response.send('k');
+ });
 });
 
 
